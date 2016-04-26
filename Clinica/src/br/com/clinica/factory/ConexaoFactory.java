76d0955db02e59acef4482e087e49462bdbf1b33
cferreira <cferreira@CIWRKST0820E02.cipucrs.pucrs.br>
2016-04-26 13:15:16 -0300

    Arrumei o erro de import.

--- Clinica/src/br/com/clinica/factory/ConexaoFactory.java
+++ Clinica/src/br/com/clinica/factory/ConexaoFactory.java
@@ -1,28 +1,32 @@
 package br.com.clinica.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import br.com.clinica.factory.ConexaoFactory;
-//import br.com.clinica.factory.Connection;
-//import br.com.clinica.factory.SQLException;
-//import br.com.clinica.factory.String;
 
 public class ConexaoFactory {
 	static final String USUARIO = "root";
 	static final String SENHA = "022010";
 	static final String URL = "jdbc:sqljdbc://localhost:3306/clinica";
 
-	//Quem chamar a conexao vai ter que fazer tratamento, por isso usamos throws
- 	public static Connection conectar() throws SQLException {
+	// Quem chamar a conexao vai ter que fazer tratamento, por isso usamos
+	// throws
+	public static Connection conectar() throws SQLException {
 		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
 		return conexao;
 	}
- 	
- 	//teste conexao
- 	
- 	public static void main(String[] args) {
- 		try{
-		Connection conexao = ConexaoFactory.conectar();
-		System.out.println("Conexão realizada com sucesso!");
- 		}catch (SQLException ex){
- 			ex.printStackTrace(); //rastrear os erros
- 			System.out.println("Não foi possivel realizar a conexão!");
- 		}
+
+	// teste conexao
+
+	public static void main(String[] args) {
+		try {
+			Connection conexao = ConexaoFactory.conectar();
+			System.out.println("Conexão realizada com sucesso!");
+		} catch (SQLException ex) {
+			ex.printStackTrace(); // rastrear os erros
+			System.out.println("Não foi possivel realizar a conexão!");
+		}
+	}
+}
