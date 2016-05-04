235d7c2a3e5d668587a182c3c610db44d78aeef2
jberte <jberte@CIWRKST0820E03.cipucrs.pucrs.br>
2016-05-04 17:05:43 -0300

    criado dominio de plano de saude

---
+++ Clinica/src/br/com/clinica/domain/PlanoDeSaude.java
@@ -0,0 +1,19 @@
+package br.com.clinica.domain;
+
+public class PlanoDeSaude {
+	private Long idPlano;
+	private String nome;
+	
+	public Long getIdPlano() {
+		return idPlano;
+	}
+	public void setIdPlano(Long idPlano) {
+		this.idPlano = idPlano;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
