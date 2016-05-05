12f2c5a1e4e4c1bf457a58e345a4e199764ec4ff
jberte <jberte@CIWRKST0820E03.cipucrs.pucrs.br>
2016-05-05 15:00:54 -0300

    Criado dominio de Consulta e Usuario

---
+++ Clinica/src/br/com/clinica/domain/Consulta.java
@@ -0,0 +1,48 @@
+package br.com.clinica.domain;
+
+import java.util.Date;
+
+public class Consulta {
+	private Long idConsulta;
+	private Long idMedico;
+	private Long idPaciente;
+	private Long idPlano;
+	private Date data;
+	private Date hora;
+	public Long getIdConsulta() {
+		return idConsulta;
+	}
+	public void setIdConsulta(Long idConsulta) {
+		this.idConsulta = idConsulta;
+	}
+	public Long getIdMedico() {
+		return idMedico;
+	}
+	public void setIdMedico(Long idMedico) {
+		this.idMedico = idMedico;
+	}
+	public Long getIdPaciente() {
+		return idPaciente;
+	}
+	public void setIdPaciente(Long idPaciente) {
+		this.idPaciente = idPaciente;
+	}
+	public Long getIdPlano() {
+		return idPlano;
+	}
+	public void setIdPlano(Long idPlano) {
+		this.idPlano = idPlano;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Date getHora() {
+		return hora;
+	}
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+}
---
+++ Clinica/src/br/com/clinica/domain/Usuario.java
@@ -0,0 +1,53 @@
+package br.com.clinica.domain;
+
+public class Usuario {
+	private Long idUsuario;
+	private String nome;
+	private String telefone;
+	private String rg;
+	private String endereco;
+	private String login;
+	private String senha;
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
